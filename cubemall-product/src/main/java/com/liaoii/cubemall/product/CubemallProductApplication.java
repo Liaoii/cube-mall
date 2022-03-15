7df8c546755de40d3bebd5d011a535d00349f17b
liaoxi <734802961@qq.com>
2022-03-15 22:51:51 +0800

    新建cubemall-product工程

---
+++ cubemall-product/src/main/java/com/liaoii/cubemall/product/CubemallProductApplication.java
@@ -0,0 +1,13 @@
+package com.liaoii.cubemall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubemallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubemallProductApplication.class, args);
+    }
+
+}
---
+++ cubemall-product/src/test/java/com/liaoii/cubemall/product/CubemallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaoii.cubemall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubemallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
