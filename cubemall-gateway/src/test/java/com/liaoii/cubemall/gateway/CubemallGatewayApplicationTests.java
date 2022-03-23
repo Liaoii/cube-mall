da69a32f474743f261712ffa5f49955257841262
liaoxi <734802961@qq.com>
2022-03-23 14:54:36 +0800

    创建cubemall-gateway工程

---
+++ cubemall-gateway/src/test/java/com/liaoii/cubemall/gateway/CubemallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liaoii.cubemall.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubemallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
